00847ff3ace13500f9bdb743aa371fa7a1a0a786
Philipe Cortez <philipe.sousa@locaweb.com.br>
2019-08-04 20:51:21 -0300

    comment unused methods

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -3,4 +3,18 @@ class Contact < ApplicationRecord
   validates :email, :name, uniqueness: true
 
   belongs_to :kind
+
+=begin
+  def author
+    "Philipe S. Cortez"
+  end
+
+  def as_json(options={})
+    super(
+      root: true,
+      methods: :author,
+      include: :kind
+    )
+  end
+=end
 end
